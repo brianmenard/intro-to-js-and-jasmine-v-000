f7902821467f97d9dbdc2692d68f84b7ca14a754
Brian <useyourbrian@gmail.com>
2016-04-08 20:47:20 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(iceCream){
+  return "I love " + iceCream;
+}
+
+function shouting(word){
+  return word.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
+
+function theTruth(){
+  return true;
+}
